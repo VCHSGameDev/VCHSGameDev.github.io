0b9f040a914754ca855e3575d4e3d0b9ddedb568
Nived Sudhakar <nived.s.2018@gmail.com>
2024-03-29 11:11:37 -0700

    reorganized

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,10 +4,10 @@ import "../App.css"
 
 function home() {
     return (
-        //<div className="App">
+        <div className="App">
             <header className="App-header">
                 <h1>VCHS Game Dev</h1>
             </header>
-        //</div>
+        </div>
     )
 } export default home;
